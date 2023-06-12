ccb0eea87f842a062532354759983a8b0c120ee9
Javier Abia <javiabia01@gmail.com>
2023-06-12 12:32:53 +0200

    Added data plot
     - not saves in a file yet
     - tested with small amount of data

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,22 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Set seaborn theme
+sns.set_theme(style="whitegrid")
 
 # Loading the csv file as a pandas DataFrame
 df = pd.read_csv("ping.csv")
+
+# Plot data
+g = sns.relplot(
+    data=df,
+    x="time", y="avg"
+)
+
+# Config plot
+g.set_axis_labels("Local time", "average (ms)", labelpad=10)
+g.set_xticklabels(rotation = 45)
+
+plt.show()
